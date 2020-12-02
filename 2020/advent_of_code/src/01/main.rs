26e891c9db995c784e91bcd1dfc9e0e0df2aa10c
Simon Kärrman <simonkarrman@icloud.com>
2020-12-02 10:43:47 +0100

    Add solution to the "Report Repair" problem

---
+++ 2020/advent_of_code/src/01/main.rs
@@ -0,0 +1,57 @@
+use serde::Deserialize;
+use std::fs;
+
+const TARGET: i32 = 2020;
+
+#[derive(Deserialize)]
+struct Input {
+    data: Vec<i32>,
+}
+
+fn get_data() -> Vec<i32> {
+    let path = "src/01/input.json";
+    let file_contents = fs::read_to_string(path).expect("Could not read file");
+    let input: Input = serde_json::from_str(&file_contents).expect("JSON could not be parsed");
+    input.data
+}
+
+fn search(data: &Vec<i32>, mut i: usize, term: i32) -> i32 {
+    let mut j = data.len() - 1;
+    if loop {
+        let sum = data[i] + data[j] + term;
+        if sum < TARGET {
+            i += 1;
+        } else if sum > TARGET {
+            j -= 1;
+        } else {
+            break true;
+        }
+        if i >= j {
+            break false;
+        }
+    } {
+        data[i] * data[j]
+    } else {
+        0
+    }
+}
+
+fn first_task(data: &Vec<i32>) {
+    let res = search(data, 0, 0);
+    println!("First challenge {}", res);
+}
+
+fn second_task(data: &Vec<i32>) {
+    let res = data
+        .iter()
+        .enumerate()
+        .fold(0, |s, (i, &k)| s + search(data, i, k) * k);
+    println!("Second challenge {}", res);
+}
+
+fn main() {
+    let mut data = get_data();
+    data.sort();
+    first_task(&data);
+    second_task(&data)
+}
